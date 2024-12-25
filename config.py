bf0c6b5f424944eff01413f9cef30b8d62a00ffd
alialamidie <alialamidie11@gmail.com>
2024-12-25 19:28:20 +0300

    update

---
+++ config.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+# Load .env file
+load_dotenv()
+
+# Access environment variables
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+DROPBOX_ACCESS_TOKEN = os.getenv("DROPBOX_ACCESS_TOKEN")
+ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS")
+GITHUB_REPO=os.getenv("Github_Repo")
+# Optional: Raise an error if variables are missing
+if not GITHUB_TOKEN or not DROPBOX_ACCESS_TOKEN:
+    raise ValueError("Required environment variables are missing!")
